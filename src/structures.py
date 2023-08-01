c39a823c0115d44e4bddb4bcb581342264d343f4
Zhiying Chen <zhiying.chen@youhualin.com>
2023-08-01 17:00:01 +0800

    chg:add get_car_num4specified_serve to carType

--- src/structures.py
+++ src/structures.py
@@ -19,7 +19,9 @@ class carType:
         self.prepare_dur = 0
 
         self.capable_goods = set()
-        self.max_serves = None
+        self.max_serves = 0
+
+        self.cars4serve = dict()
 
     def __repr__(self):
         return "carType(type={}, total={}, max_serves={})".format(self.type, self.total_num, self.max_serves)
@@ -32,23 +34,38 @@ class carType:
                  return_dur=self.return_dur, serve_dur=self.serve_dur, rest_dur=self.rest_dur,
                     full_dur=self.full_dur)
         car4one = oneServe.generate_min_car_num()
+        self.cars4serve[1] = car4one
+
         min_serves = math.ceil(self.total_num / car4one)
 
-        curr_needed_planes = 0
+        curr_needed_cars = 0
         serve_num = min_serves
-        while curr_needed_planes <= self.total_num:
+        while curr_needed_cars <= self.total_num:
             k = serve_num + 1
             currServe = sds.Solver(stTime=stTime, edTime=edTime, serveNum=k,
                  upload_dur=self.upload_dur, unpack_dur=self.unpack_dur,  prepare_dur=self.prepare_dur,
                 leave_dur=self.leave_dur, return_dur=self.return_dur, serve_dur=self.serve_dur, rest_dur=self.rest_dur,
                     full_dur=self.full_dur)
-            curr_needed_planes = currServe.generate_min_car_num()
-            if curr_needed_planes <= self.total_num:
+            curr_needed_cars = currServe.generate_min_car_num()
+            self.cars4serve[k] = curr_needed_cars
+            if curr_needed_cars <= self.total_num:
                 serve_num += 1
 
         self.max_serves = serve_num
         logging.info("Max serving num for plane type {} is {}".format(self.type, self.max_serves))
 
+    def get_car_num4specified_serve(self, stTime, edTime, specified_serve_num):
+        if specified_serve_num in self.cars4serve:
+            return self.cars4serve[specified_serve_num]
+        else:
+            currServe = sds.Solver(stTime=stTime, edTime=edTime, serveNum=specified_serve_num,
+                                   upload_dur=self.upload_dur, unpack_dur=self.unpack_dur, prepare_dur=self.prepare_dur,
+                                   leave_dur=self.leave_dur, return_dur=self.return_dur, serve_dur=self.serve_dur,
+                                   rest_dur=self.rest_dur,
+                                   full_dur=self.full_dur)
+            curr_needed_cars = currServe.generate_min_car_num()
+            self.cars4serve[specified_serve_num] = curr_needed_cars
+            return curr_needed_cars
 
 class goodsType:
     def __init__(self, type, frozen_dur):
